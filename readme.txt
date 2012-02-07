bb4fd35ee02f889125ba915ff678bfc3bbc410fb
Udinra <Udinra@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-07 06:32:05 +0000

    tag 1.8
    git-svn-id: https://plugins.svn.wordpress.org/udinra-all-image-sitemap/tags/1.8@501507 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Udinra All Image Sitemap ===
+Contributors: Udinra
+Donate link: http://udinra.com/
+Tags: xml sitemaps, google sitemaps, image sitemap, seo, search engines, sitemap
+Requires at least: 2.0
+Tested up to: 3.3.1
+Stable tag: 1.8
+
+Automatically creates Google XML sitemap for images and pings Google,Bing and Ask.com
+
+== Description ==
+
+Automatically creates Google XML sitemap for images and pings Google,Bing and Ask.com.
+
+You can find list of features and comparison with other image sitemaps at <a href="http://udinra.com/blog/udinra-image-sitemap">Udinra Image Sitemap features</a>
+
+All information about the plugin will be updated on Wordpress as well as the link shared above.If you find any bugs then report it either on wordpress or on the link shared above.
+
+== Installation ==
+
+Here's how you can install the plugin:
+
+http://udinra.com/blog/udinra-image-sitemap 
+
+== Frequently Asked Questions ==
+
+http://udinra.com/blog/udinra-image-sitemap
+
+== Screenshots ==
+
+http://udinra.com/blog/udinra-image-sitemap
+
+== Changelog ==
+
+= 1.8 =
+
+Fixed special characters in title and caption issue.
+
+= 1.7 =
+
+Final version of plugin (with all features and no bug).New version may come up in case new requirements are requested by users
+
+== Upgrade Notice ==
+
+Not needed.
---
+++ udinra-all-image-sitemap.php
@@ -0,0 +1,176 @@
+<?php
+/*
+Plugin Name: Udinra All Image Sitemap 
+Plugin URI: http://udinra.com/blog/udinra-image-sitemap
+Description: Automatically generates Google Image Sitemap and submits it to Google,Bing and Ask.com.
+Author: Udinra
+Version: 1.8
+Author URI: http://udinra.com
+*/
+
+function UdinraAA_IMS() {
+if(!isset($_POST['udinra_ping_google'])){
+$_POST['udinra_ping_google'] = "";
+}
+if(!isset($_POST['udinra_ping_bing'])){
+$_POST['udinra_ping_bing'] = "";
+}
+if(!isset($_POST['udinra_ping_ask'])){
+$_POST['udinra_ping_ask'] = "";
+}
+if($_POST['udinra_img_site']){
+update_option('udinra_ping_google',$_POST['udinra_ping_google']);
+update_option('udinra_ping_bing',$_POST['udinra_ping_bing']);
+update_option('udinra_ping_ask',$_POST['udinra_ping_ask']);
+}
+$wp_udinra_ping_google = get_option('udinra_ping_google');
+$wp_udinra_ping_bing  = get_option('udinra_ping_bing');
+$wp_udinra_ping_ask = get_option('udinra_ping_ask');
+$udinra_sitemap_response = "";
+if(isset($_POST['udinra_img_site'])){
+$udinra_sitemap_response = udinra_image_sitemap_loop(); 
+}
+?>
+<div class="wrap">
+<h2>Udinra All Image Sitemap (Configuration)</h2>
+<form method="post" id="UdinraAA_IMS">
+<fieldset class="options">
+<p><input type="checkbox" id="udinra_ping_google" name="udinra_ping_google" value="udinra_ping_google" <?php if($wp_udinra_ping_google == true) { echo('checked="checked"'); } ?> />Ping Google (Recommended)</p>
+<p><input type="checkbox" id="udinra_ping_bing" name="udinra_ping_bing" value="udinra_ping_bing" <?php if($wp_udinra_ping_bing == true) { echo('checked="checked"'); } ?> />Ping Bing (Recommended)</p>
+<p><input type="checkbox" id="udinra_ping_ask" name="udinra_ping_ask" value="udinra_ping_ask" <?php if($wp_udinra_ping_ask == true) { echo('checked="checked"'); } ?> />Ping Ask.com (Recommended)</p>
+<p><em>If you have a minute, please <a href="http://wordpress.org/extend/plugins/udinra-all-image-sitemap/" target="_blank">rate this plugin</a> on WordPress.org... thanks!</em></p>
+<p><input type="submit" name="udinra_img_site" value="Create Sitemap" /></p>
+<p><?php echo "Status:"."<br><br>".$udinra_sitemap_response; ?></p>
+</fieldset>
+</form>
+<p>If you face any problem then create blank sitemap-image.xml file in your Wordpress root directory and make it writable</p>
+<h3>Suggestions?</h3>
+<p>Please email your suggestions to <a href="http://udinra.com/contact-me">Udinra</a></p>
+<p>Are You Making Money With Your Images then Donate Us a small share.</p>
+<p>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">  
+ <input type="hidden" name="business" value="pitaji@udinra.com">  
+ <input type="hidden" name="cmd" value="_donations">  
+ <input type="hidden" name="item_name" value="udinra">  
+ <input type="hidden" name="item_number" value="Udinra Image Sitemap plugin">  
+ <input type="hidden" name="currency_code" value="USD">  
+ <input type="image" name="submit" border="0" 
+        src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif"  
+        alt="PayPal - The safer, easier way to pay online">  
+ <img alt="" border="0" width="1" height="1" src="https://www.paypal.com/en_US/i/scr/pixel.gif" >  
+</form>
+</p>
+</div>
+<?php
+
+}
+
+function udinra_image_sitemap_loop() {
+
+$wp_udinra_ping_google = get_option('udinra_ping_google');
+$wp_udinra_ping_bing  = get_option('udinra_ping_bing');
+$wp_udinra_ping_ask = get_option('udinra_ping_ask');
+
+global $wpdb;
+
+$udinra_posts = $wpdb->get_results("SELECT post_title,post_excerpt,post_parent,guid	FROM $wpdb->posts
+				 			WHERE post_type = 'attachment'
+							AND post_mime_type like 'image%'
+							AND post_parent > 0
+							ORDER BY post_date desc");
+
+if (empty ($udinra_posts)) {
+	return false;
+
+} else {
+	$udinra_xml   = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+	$udinra_xml  .= '<!-- Generated-by Udinra Image Sitemap (http://udinra.com) -->' . "\n";
+	$udinra_xml  .= '<!-- Generated-on="' . date("F j, Y, g:i a") .'" -->' . "\n";		     
+	$udinra_xml  .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">' . "\n";
+	
+	$udinra_cur_post_id= 0;
+	$udinra_prev_post_id= 0;		
+	$udinra_first_time = 0;
+
+	foreach ($udinra_posts as $udinra_post) { 
+		$udinra_cur_post_id= $udinra_post->post_parent;
+		if($udinra_cur_post_id != $udinra_prev_post_id) {
+  			$udinra_post_url = get_permalink($udinra_cur_post_id);
+			if($udinra_first_time == 1) {
+				$udinra_xml .= "\t"."</url>"."\n"; 
+				$udinra_first_time = 0;
+			}
+			$udinra_xml .= "\t"."<url>"."\n";
+			$udinra_xml .= "\t\t"."<loc>".$udinra_post_url."</loc>"."\n";
+			$udinra_xml .= "\t\t"."<image:image>"."\n";
+			$udinra_xml .= "\t\t\t"."<image:loc>".$udinra_post->guid."</image:loc>"."\n";
+			$udinra_xml .= "\t\t\t"."<image:caption>".htmlspecialchars($udinra_post->post_excerpt)."</image:caption>"."\n";
+			$udinra_xml .= "\t\t\t"."<image:title>".htmlspecialchars($udinra_post->post_title)."</image:title>"."\n";
+			$udinra_xml .= "\t\t"."</image:image>"."\n";
+			$udinra_first_time = 1;
+			$udinra_prev_post_id = $udinra_cur_post_id;
+		}
+		else {
+			$udinra_xml .= "\t\t"."<image:image>"."\n";
+			$udinra_xml .= "\t\t\t"."<image:loc>".$udinra_post->guid."</image:loc>"."\n";
+			$udinra_xml .= "\t\t\t"."<image:caption>".htmlspecialchars($udinra_post->post_excerpt)."</image:caption>"."\n";
+			$udinra_xml .= "\t\t\t"."<image:title>".htmlspecialchars($udinra_post->post_title)."</image:title>"."\n";
+			$udinra_xml .= "\t\t"."</image:image>"."\n";
+		}
+	} 
+	$udinra_xml .= "\t"."</url>"."\n";
+	$udinra_xml .= "</urlset>";
+
+	$udinra_image_sitemap_url = $_SERVER["DOCUMENT_ROOT"] . '/sitemap-image.xml';
+
+	if (IsImageSitemapWritable($_SERVER["DOCUMENT_ROOT"]) || IsImageSitemapWritable($udinra_image_sitemap_url)) {
+		if (file_put_contents ($udinra_image_sitemap_url, $udinra_xml)) {
+			$udinra_tempurl = get_bloginfo('url'). '/sitemap-image.xml';
+			$udinra_sitemap_response = "Sitemap created successfully"."<br>";
+			
+			if ($wp_udinra_ping_google == true) {
+				$udinra_ping_url ='';
+				$udinra_ping_url = "http://www.google.com/webmasters/tools/ping?sitemap=" . urlencode($udinra_tempurl);
+				$udinra_response = wp_remote_get( $udinra_ping_url );
+				if($udinra_response['code']=200)
+				{ $udinra_sitemap_response .= "Pinged Google Successfully"."<br>"; }
+				else { $udinra_sitemap_response .= "Failed to ping Google.Please submit your image sitemap(sitemap-image.xml) at Google Webmaster.";}}
+			if ($wp_udinra_ping_bing == true) {
+				$udinra_ping_url ='';
+				$udinra_ping_url = "http://www.bing.com/webmaster/ping.aspx?sitemap=" . urlencode($udinra_tempurl);
+				$udinra_response = wp_remote_get( $udinra_ping_url );
+				if($udinra_response['code']=200)
+				{ $udinra_sitemap_response .= "Pinged Bing Successfully"."<br>"; }
+				else { $udinra_sitemap_response .= "Failed to ping Bing.Please submit your image sitemap(sitemap-image.xml) at Bing Webmaster.";}}
+			if ($wp_udinra_ping_ask == true) {
+				$udinra_ping_url ='';
+				$udinra_ping_url = "http://submissions.ask.com/ping?sitemap=" . urlencode($udinra_tempurl);
+				$udinra_response = wp_remote_get( $udinra_ping_url );
+				if($udinra_response['code']=200)
+				{ $udinra_sitemap_response .= "Pinged Ask.com Successfully"."<br>"; }
+				else { $udinra_sitemap_response .= "Failed to ping Ask.com."; }}
+		}
+	}
+   }
+return $udinra_sitemap_response;
+}
+
+function udinra_image_sitemap_admin() {
+	if (function_exists('add_options_page')) {
+	add_options_page('Udinra Image Sitemap', 'Udinra Image Sitemap', 'manage_options', basename(__FILE__), 'UdinraAA_IMS');
+	}
+}
+
+function IsImageSitemapWritable($udinra_filename) {
+	if(!is_writable($udinra_filename)) {
+		$udinra_sitemap_response = "The file sitemap-image.xml is not writable please check permission of the file for more details visit http://udinra.com/blog/udinra-image-sitemap";
+		return false;
+	}
+	return true;
+}
+
+add_action ('publish_post','udinra_image_sitemap_loop');
+add_action ('publish_page','udinra_image_sitemap_loop');
+add_action('admin_menu','udinra_image_sitemap_admin');
+
+?>
